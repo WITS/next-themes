b3ab496434c534dc0b42fe17500a00f398dc435c
Ian Jones <ian.jones@vercel.com>
2022-12-19 16:44:38 -0500

    fixed typo

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,5 @@ const client = require('./client')
 const server = require('./server')
 
 export const useTheme = client.useTheme as () => UseThemeProps;
-export const ThemeProvder = client.ThemeProvder as FC<ThemeProviderProps>;
+export const ThemeProvider = client.ThemeProvider as FC<ThemeProviderProps>;
 export const ServerThemeProvider = server.ServerThemeProvider as FC<ThemeProviderProps>;
