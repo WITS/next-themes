7a6852a79fbd44d8ee5e965e6a0dd04b1c821526
Ian Jones <ian.jones@vercel.com>
2022-12-19 16:20:01 -0500

    updated to newer version of rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,7 +1,7 @@
-import commonjs from '@rollup/plugin-commonjs';
-import external from 'rollup-plugin-peer-deps-external';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
 
 export default [
   {
@@ -34,30 +34,14 @@ export default [
     ],
   },
   {
-    inlineDynamicImports: true,
     input: 'src/index.ts',
     output: {
       file: 'dist/index.js',
       format: 'cjs',
     },
     plugins: [
-      external(),
-      resolve(),
       commonjs(),
       typescript(),
     ],
   },
-  // {
-  //   input: 'main-b.js',
-  //   output: [
-  //     {
-  //       file: 'dist/bundle-b1.js',
-  //       format: 'cjs'
-  //     },
-  //     {
-  //       file: 'dist/bundle-b2.js',
-  //       format: 'es'
-  //     }
-  //   ]
-  // },
-];
+]
